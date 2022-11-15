0666a6ef3030824b87beea42b277d45a9b27c447
xxxxfault <jiaqian0730@gmail.com>
2022-11-15 13:06:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import HelloController
+    from "./controllers/hello-controller.js"
+import UserController
+    from "./controllers/users/users-controller.js"
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js"
+const app = express()
+app.use(express.json())
+app.use(cors())
+HelloController(app)
+UserController(app)
+TuitsController(app)
+app.listen(process.env.PORT || 4000);
